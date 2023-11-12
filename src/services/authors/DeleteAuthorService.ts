3609c5939d6fec7476f29d9d44cacde0054b6c27
Farioso Fernando <farioso.f@gmail.com>
2023-11-12 21:18:14 +0100

    feat: delete account feature

---
+++ src/services/authors/DeleteAuthorService.ts
@@ -0,0 +1,21 @@
+import database from "../../prisma";
+
+export default class DeleteAuthorService {
+    async execute(id: string) {
+
+        const author = await database.author.findFirst({ where: { id: id } });
+
+        if (!author) throw new Error("Id invalido.");
+
+        const deleteAction = await database.author.delete({
+            where: { id: id },
+            select: {
+                id: true,
+                username: true,
+                email: true,
+            },
+        });
+
+        return deleteAction;
+    }
+}
